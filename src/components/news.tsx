0be3449d8a431c264a1094c7f041ef944af976d9
arnavpadwal <arnav.padwal7@gmail.com>
2025-05-20 00:21:48 +0530

    clean

--- src/components/news.tsx
+++
@@ -1,3 +0,0 @@
-export default function News() {
-  return <div className="h-[640px] w-[265px] bg-white absolute right-5"></div>;
-}
--- src/components/stats.tsx
+++
@@ -1,7 +0,0 @@
-export default function Stats() {
-  return (
-    <div className="relative mx-auto max-w-[1830px] w-full h-[241px] mt-[690px] bg-white">
-      {/* Your stats content goes here */}
-    </div>
-  );
-}
